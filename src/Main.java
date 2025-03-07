f9ec1b670d19a0c1924cdfbcfbb02fb7a0b427c0
Marc0Medina <medina.marco.antonio99@gmail.com>
2025-03-07 10:49:33 -0600

    Segunda version dibujar cuadrado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+
+        do {
+            System.out.print("Ingresa un número para el tamaño del cuadrado: ");
+            n = scanner.nextInt();
+
+            if (n <= 0) {
+                System.out.println("El tamaño debe ser entero positivo.");
+            } else {
+                if (CuadradoAsteriscos.visible) {
+                    CuadradoAsteriscos.dibujarCuadrado(n);
+                } else {
+                    System.out.println("El cuadrado es invisible.");
+                }
+            }
+
+            System.out.print("¿Quieres cambiar el tamaño (s/n)? ");
+            char yn = scanner.next().charAt(0);
+
+            if (yn == 'n') {
+                System.out.println("¡Listo, papi!");
+                break;
+            }
+
+            System.out.print("¿Quieres hacerlo visible o invisible (v/i)? ");
+            char vi = scanner.next().charAt(0);
+            CuadradoAsteriscos.visible = (vi == 'v');
+
+        } while (true);
+        scanner.close();
     }
 }
